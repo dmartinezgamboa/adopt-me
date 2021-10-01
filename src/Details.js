7467804555703e4a410c9b6da0a96a0518eed0a2
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-01 16:02:42 -0700

    Implement loading ... State

--- src/Details.js
+++ src/Details.js
@@ -11,22 +11,28 @@ class Details extends Component {
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
-    this.setState({
-      loading: false,
-      name: json.pets[0].name,
-      breed: json.pets[0].breed,
-      animal: json.pets[0].animal,
-    });
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
   }
   render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
     const { animal, breed, city, state, description, name } = this.state;
     return (
       <div className="details">
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-          <p>{description}</p>
           <button>Adopt {name}</button>
+          <p>{description}</p>
         </div>
       </div>
     );
